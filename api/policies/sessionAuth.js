5c4de089d935062671aa004c6a20b84e300a2555
Rajat Gupta <rajatgupta0889@gmail.com>
2017-02-25 17:25:08 +0530

    add policy code

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -7,15 +7,74 @@
  * @docs        :: http://sailsjs.org/#!documentation/policies
  *
  */
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
 
-  // User is allowed, proceed to the next policy, 
+  // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
-  if (req.session.authenticated) {
-    return next();
+  var apiKey = req.headers.apikey;
+  token = req.headers.token;
+
+
+  if (apiKey === "$2a$10$.CPkZU3.R3bRE3bDI5epUuuHrUe63EbqW7HhhOriSj5beFwTdT16W") {
+    Token.findOne({id: token}, function (err, tokenDetails) {
+      if (!err) {
+        if (tokenDetails) {
+
+          chkExpiredToken(tokenDetails, function (err, isTokenExpired) {
+            if (!err) {
+              if (!(isTokenExpired)) {
+                req.user_details = tokenDetails.user_details;
+                next();
+              } else {
+                return res.status(401).json({"message": "Token is expired"});
+              }
+            } else {
+              return res.json(err);
+            }
+          });
+
+        } else {
+          return res.status(401).json({"message": "user token is either not valid or expired"});
+        }
+      } else {
+        sails.config.globals.logger.error(err);
+        return res.json(err);
+      }
+    });
+  } else {
+    return res.status(401).json({"message": "API key is not valid"});
   }
+
+
+  // User is allowed, proceed to the next policy,
+  // or if this is the last policy, the controller
+  /*if (req.session.user_details) {
+   return next();
+   }*/
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You are not permitted to perform this action.');
+  //return res.forbidden('You are not permitted to perform this action.');
+  //return res.status(401).json({"message" : "user is not logged in"});
+
+};
+
+
+var chkExpiredToken = function (tokenDetail, cb) {
+  var currentDateTime = new Date();
+  var tokenExpireTime = tokenDetail.expire;
+
+  if (tokenExpireTime > currentDateTime) {
+    //not expired
+    return cb(null, false);
+  } else {
+    Token.destroy({id: tokenDetail.id}, function (err, result) {
+      if (!err) {
+        return cb(null, true);
+      } else {
+        sails.config.globals.logger.error(err);
+        return cb(null, true);
+      }
+    });
+  }
 };
